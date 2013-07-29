169492c40e111f7f752341b929b7c330b1e22ffa
Raghu Betina <rbetina@gmail.com>
2013-07-29 15:30:57 -0500

    rails new, added starter_generators

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TeachingApp::Application.config.secret_key_base = '79c11028bab329ffa0583374aae32e2cd3a8eb5b78a7107f80861d22309b5d8c7a0ea1010f966c33351ceeaf5287ad99bfc244418b693f127554c9fafff1d803'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TeachingApp::Application.config.session_store :cookie_store, key: '_teaching_app_session'
